38118b78897272339caf6d436352d6d86e8c8807
gtg506q <gtg506q@gmail.com>
2014-09-24 01:50:49 -0400

    Merge branch 'master' of git://github.com/allencct/Dvorak
    Conflicts:
    	content/command_center.php

--- content/command_center.php
+++ content/command_center.php
@@ -395,67 +395,87 @@ $colony->update_resources();
 				$('#'+ name +'_screen').html('');
 				
 				// Populate the content of this screen.
-				var tiles_data = json_data.tiles_data;
+				var nearest_tiles = get_nearest_tiles(json_data.tiles_data)
+				draw_map(nearest_tiles, name);
+				// var tiles_data = json_data.tiles_data;
 				// TODO: figure out how to _actually_ iterate over the axial coordinate system.
 				//		 go here and search 'axial coordinates':
 				//		 http://www.redblobgames.com/grids/hexagons/
-				// center_tile_x, center_tile_y are x,y of player home base tile (in database?)
 				
-				// x,y offsets for start of map area
-				var div_y_offset=100;
-				var div_x_offset=177;
-				var rel_x=0;
-				var rel_y=-2;
-
-				// 19 map tiles are shown at once, center tile is #10
-				// 1st tile is (center-0,center-2)
-				for (var i=0; i<19; i++) 
-				{
-					if (i==3) {rel_x+=4; rel_y++;}
-					else if (i==7) {rel_x+=5; rel_y++;}
-					else if (i==12) {rel_x+=5; rel_y++;}
-					else if (i==16) {rel_x+=4; rel_y++;}
-
-					var div=jQuery('<img>', {
-						"id": 'map_tile'+ i + 'div',
-						"class": 'map_tile_div',
-						"src": 'media/images/banana.png',
-						"title": '(' + (center_tile_x+i-rel_x) + ',' + (center_tile_y+rel_y) + ')',
-						"x": (center_tile_x+i-rel_x),
-						"y": (center_tile_y+rel_y)
-					});
-					if (i<3) div.offset({top:div_y_offset,left:div_x_offset});
-					else if (i<7) div.offset({top:div_y_offset+60,left:div_x_offset-245});
-					else if (i<8) div.offset({top:div_y_offset+121,left:div_x_offset-560});
-					else if (i<12) div.offset({top:div_y_offset+40,left:div_x_offset});
-					else if (i<16) div.offset({top:div_y_offset+101,left:div_x_offset-316});
-					else div.offset({top:div_y_offset+81,left:div_x_offset-177});
-					div.appendTo('#'+ name +'_screen');
-					/*$(document).ready(function() {
-						div.bind('mouseover mouseout click', function(event) {
-    					var $tgt = $(event.target);
-    					if (!$tgt.closest('.syntax_hilite').length) {
-      						$tgt.toggleClass(event.type == 'click' ? 'outline-element-clicked' : 'outline-element');
-      						alert(div)
-    						}
-    					//alert($tgt);
-  						});
-					});*/
-					
-					/*$('#map_tile'+ i).hover(function() {
-						$(this).toggleClass('highLight')
-						//$("p").css("background-color","yellow");
-						// display this tile's info in the hover-over info box.
-						// (see UI mock-up)
-					});*/
-				}
-				// Display this screen.
-				change_screen(name);
-			}   
+			}
 			else
 				alert(json_data.ERROR);
 		});
 	});
+
+	function draw_map(nearest_tiles, name) {
+		// center_tile_x, center_tile_y are x,y of player home base tile (in database?)
+		// screen x,y offsets for top left of map area
+		var div_y_offset=100;
+		var div_x_offset=260;
+		// used to number tiles with coordinates
+		var rel_x=0;
+		var rel_y=-2;
+
+		// 22 map tiles are shown at once, center tile is 12th tile (i=11)
+		// 1st tile is (center-0,center-2)
+		// adds map tile divs to map
+		for (var i=0; i<23; i++)
+		{
+			// sets up tile offsets and coordinates
+			if (i==4) {div_y_offset-=93; div_x_offset-=330; rel_x=-1; rel_y++;}
+			else if (i==7) {div_y_offset-=61; div_x_offset+=588;}
+			else if (i==9) {div_y_offset-=124; div_x_offset-=396; rel_x=-2; rel_y++;}
+			else if (i==14) {div_y_offset-=184; div_x_offset+=192; rel_x=-3; rel_y++;}
+			else if (i==19) {div_y_offset-=93; div_x_offset-=330; rel_x=-3; rel_y++;}
+			else if (i==21) {div_y_offset-=61; div_x_offset+=378;}
+
+			// actually create divs with initial 'empty' tile image
+			var div=jQuery('<img>', {
+				"id": 'map_tile'+ i + 'div',
+				"class": 'map_tile_div',
+				"src": 'media/themes/default/images/tile_empty.png',
+				"title": '(' + (center_tile_x+rel_x) + ',' + (center_tile_y+rel_y) + ')',
+				"x": (center_tile_x+rel_x),
+				"y": (center_tile_y+rel_y)
+			});
+
+			// set position of tile
+			div.offset({top:div_y_offset,left:div_x_offset});
+
+			// adds tile to map div
+			div.appendTo('#'+ name +'_screen');
+
+			// move tile offsets down and to the right
+			div_x_offset-=18;
+			div_y_offset+=31;
+			rel_x++;
+
+			// highlight map tile on hover
+			div.on('mouseover', function() {$(this).css("background-color", "yellow");});
+			div.on('mouseout', function() {$(this).css("background-color", "");});
+		}
+		// Display this screen.
+		change_screen(name);
+	}
+
+	function get_nearest_tiles(tiles_data) {
+		var nearest_tiles;
+		/*colony @ center_tile_x,center_tile_y
+		$tile_data = array();
+		$tile_data['id'] = $tile_cache['tile_id'];
+		$tile_data['player_has_vision'] = $tile_cache['player_has_vision'];
+		$tile_data['cache_time'] = $tile_cache['cache_time'];
+		$tile_data['coord_x'] = $tiles[$tile_cache['tile_id']]['coord_x'];
+		$tile_data['coord_y'] = $tiles[$tile_cache['tile_id']]['coord_y'];
+		$tiles_data[$tile_data['id']] = $tile_data;*/
+		var x_min = center_tile_x;
+		for (var tile in tiles_data) {
+			
+		}
+
+		return nearest_tiles;
+	}
 	
 	// This function manages the visual hiding and showing of the screens.
 	// DO NOT EDIT THIS FUNCTION
